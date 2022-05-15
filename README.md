3d4f9620714a0ab9d3b1a97ce6d644f341097b0e
Abuton <alaroabubakarolayemi@yahoo.com>
2022-05-15 11:31:57 +0400

    feat: refactored app file

--- README.md
+++ README.md
@@ -7,21 +7,32 @@
 
 # Loan Eligibility Predictor
 
-A Machine Learning Model to predict Loan eligibility for Dream Housing Finance company
+A Machine Learning Model to predict Loan Eligibility for Dream Housing Finance company
 
 Dream Housing Finance company deals in all kinds of home loans. They have presence across all urban, semi urban and rural areas. Customer first applies for home loan and after that company validates the customer eligibility for loan.
 
 Company wants to automate the loan eligibility process (real time) based on customer detail provided while filling online application form. These details are Gender, Marital Status, Education, Number of Dependents, Income, Loan Amount, Credit History and others. To automate this process, they have provided a dataset to identify the customers segments that are eligible for loan amount so that they can specifically target these customers.
 
-## Streamlit App
+## Loan Eligibity Prediction App
 
-Streamlit App was Built to Expose the Model into production
+The App was Built to Expose the Model into production -  a stage called deployement in Machine Learning
 The following Steps were taking
 
-- Model was Built and was pickled using the sklearn library
+- Model was Built using the Random Forest Classifier
+- The model was then pickled using the pickle library
 - A frontend view was designed to collect input data using streamlit
-- Web App was deployed using Streamlit Sharing
+- A Web App was deployed using Streamlit Sharing and heroku
 
 Data Source [here](https://datahack.analyticsvidhya.com/contest/practice-problem-loan-prediction-iii/?utm_source=blog&utm_medium=model_depoyment_using_streamlit#ProblemStatement)
 
+## How to use the App
+
 ![image](https://user-images.githubusercontent.com/40719064/125074167-d491eb80-e0b4-11eb-8021-97d465070e33.png)
+
+The Above Image is the front end design of the App. It has the following fields
+
+1. Gender - A select box to choose the gender of the loan applicant
+2. Monthly Income - An Input box to enter the monthly income of the applicant
+3. Credit History - A select box to chose if the applicant has unclear debt or not
+4. Marital Status - A select box to choose if the applicant is married
+5. Loan Amount - A number input inbox to enter the amount an applicant wants to borrowed
--- app.py
+++ app.py
@@ -1,80 +1,92 @@
 import pickle
 import streamlit as st
- 
 
-st.set_page_config(page_title='Streamlit Model Deployment', page_icon=':moneybag:',
-                   layout="wide")
+
+st.set_page_config(
+    page_title="Loan Eligibility Predition", page_icon=":moneybag:", layout="wide"
+)
 
 # loading the trained model
-def load_model(model_path:str):
-    pickled_model = open(model_path, 'rb') 
+def load_model(model_path: str):
+    pickled_model = open(model_path, "rb")
     classifier = pickle.load(pickled_model)
     return classifier
-  
-# defining the function which will make the prediction using the data which the user inputs 
-def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
+
+
+# defining the function which will make the prediction using the data which the user inputs
+def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):
 
     classifier = load_model("model/loan_predictor_model.pkl")
- 
-    # Pre-processing user input    
-    if Gender == "Male": 
+
+    # Pre-processing user input
+    if Gender == "Male":
         Gender = 0
     else:
         Gender = 1
- 
+
     if Married == "Unmarried":
         Married = 0
     else:
         Married = 1
- 
+
     if Credit_History == "Unclear Debts":
         Credit_History = 0
     else:
-        Credit_History = 1  
- 
+        Credit_History = 1
+
     LoanAmount = LoanAmount / 1000
- 
-    # Making predictions 
-    prediction = classifier.predict( [[Gender, Married, ApplicantIncome,
-                             LoanAmount, Credit_History]])
-     
+
+    # Making predictions
+    prediction = classifier.predict(
+        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]]
+    )
+
     if prediction == 0:
-        pred = 'Rejected'
+        pred = "Rejected"
     else:
-        pred = 'Approved'
+        pred = "Approved"
     return pred
-      
-  
-# this is the main function in which we define our webpage  
-def main():       
-    # front end elements of the web page 
-    html_temp = """ 
-    <div style ="background-color:#2a3990;padding:15px"> 
-    <h2 style ="color:#ffff00;text-align:center;">Bank Loan Prediction</h2> 
-    </div> 
+
+
+# this is the main function in which we define our webpage
+def main():
+    # front end elements of the web page
+    html_temp = """
+    <div style ="background-color:#2a3990;padding:15px">
+    <h2 style ="color:#ffff00;text-align:center;">Bank Loan Eligibility Prediction</h2>
+    </div>
     """
-      
+
     # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html = True) 
-      
-    # following lines create boxes in which user can enter data required to make prediction 
-    left_column, right_column = st.beta_columns([0.5, 0.5])
-    Gender = left_column.selectbox('Gender',("Male","Female"))
-    Married = right_column.selectbox('Marital Status',("Unmarried","Married")) 
-    ApplicantIncome = left_column.number_input("Applicant monthly income ($)") 
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    # following lines create boxes in which user can enter data required to make prediction
+    left_column, right_column = st.columns([0.5, 0.5])
+    Gender = left_column.selectbox("Gender", ("Male", "Female"))
+    Married = right_column.selectbox("Marital Status", ("Unmarried", "Married"))
+    ApplicantIncome = left_column.number_input("Applicant monthly income ($)")
     LoanAmount = right_column.number_input("Total loan amount ($)")
-    Credit_History = st.selectbox('Credit History',("Unclear Debts","No Unclear Debts"))
-    result =""
-      
-    # when 'Predict' is clicked, make the prediction and store it 
-    if st.button("Predict "): 
-        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
-        if result == 'Rejected':
-            st.error(f'Loan is {result}\n ## Sorry ** :sob: ** ')
+    Credit_History = st.selectbox(
+        "Credit History", ("Unclear Debts", "No Unclear Debts")
+    )
+    result = ""
+
+    # when 'Predict' is clicked, make the prediction and store it
+    if st.button("Predict "):
+        result = prediction(
+            Gender, Married, ApplicantIncome, LoanAmount, Credit_History
+        )
+        if result == "Rejected":
+            st.error(f"Loan is {result}\n ## Sorry ** :sob: ** ")
 
         else:
-            st.success('loan is {}\n ## Congratulations ** :thumbsup: :dollar: **'.format(result))
-            st.balloons();
-     
-if __name__=='__main__': 
+            st.success(
+                "loan is {}\n ## Congratulations ** :thumbsup: :dollar: **".format(
+                    result
+                )
+            )
+            st.balloons()
+
+
+if __name__ == "__main__":
     main()
